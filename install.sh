a922795cfeb32f59bc6b4c2035908e590d605e30
janardhanputta <37863509+janardhanputta@users.noreply.github.com>
2018-05-27 18:46:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo cp /home/ubuntu/target/myweb.war /opt/apache-tomcat-7.0.85/webapps/
-sudo cp /home/ubuntu/src/main/webapp/index.jsp /opt/apache-tomcat-7.0.85/webapps/myweb/
+sudo cp /home/ubuntu/target/myweb.war /home/ubuntu/apache-tomcat-7.0.88/webapps/
+sudo cp /home/ubuntu/src/main/webapp/index.jsp /home/ubuntu/apache-tomcat-7.0.88/webapps/myweb/
